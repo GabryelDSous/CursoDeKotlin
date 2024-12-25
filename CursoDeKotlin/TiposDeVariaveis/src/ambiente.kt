618d079427182520d7cce082a5cb391bd5a694cc
unknown <gabryelpro260406@gmail.com>
2024-12-25 19:08:27 -0300

    M01A018

--- CursoDeKotlin/TiposDeVariaveis/src/ambiente.kt
+++ CursoDeKotlin/TiposDeVariaveis/src/ambiente.kt
@@ -1,10 +1,18 @@
+    //n.inc() // para funcionar tem que ser n = n.inc()
 fun main(args: Array<String>) {
-    val n1: Int = 5
-    val n2: Int = 5
-    println("$n1 maior que $n2? ${n1>n2}")
-    println("$n1 menor que $n2? ${n1<n2}")
-    println("$n1 maior ou igual a $n2? ${n1>=n2}")
-    println("$n1 menor ou igual a que $n2? ${n1<=n2}")
-    println("$n1 igual a $n2? ${n1==n2}")
-    println("$n1 diferente de $n2? ${n1!=n2}")
+    var a = 3
+    var b = 4
+    var c = 5
+
+    var res = a++ + --b + ++c
+
+    println("O valores são $a, $b e $c")
+    println("O resultado é $res")
 }
+/*
+    a = 4
+    b = 3
+    c = 6
+
+    res = 12
+ */
