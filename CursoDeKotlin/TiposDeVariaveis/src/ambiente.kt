571c997f89a0560bb8093fd8a23479aed4fe9f7b
unknown <gabryelpro260406@gmail.com>
2024-12-25 19:46:24 -0300

    Números aleatorios

--- CursoDeKotlin/TiposDeVariaveis/src/ambiente.kt
+++ CursoDeKotlin/TiposDeVariaveis/src/ambiente.kt
@@ -1,14 +1,4 @@
-import kotlin.math.r
 fun main(args: Array<String>) {
-    var v1 = 5
-    var v2 = 8
-    var v3 = 3
-    var v4 = 2
-
-    var res = v1 < v2 && !(v3 - 1 != v1 / v4)
-
-    println("O resultado da expressão é [$res]")
+    var n:Int = (0..50).random()
+    println("O valor sortado foi o $n")
 }
-/*
-    false
- */
